8e5205772895705b4c2c512b0b70ac3336c4f3ea
Gabriel Badaró <grbadas@gmail.com>
2023-03-23 20:13:49 -0300

    Oauth changes

--- spring-store-backend/spring-store/src/main/java/com/badas/springboot/controller/LoginController.java
+++
@@ -1,5 +0,0 @@
-package com.badas.springboot.controller;
-
-public class LoginController {
-
-}
---
+++ spring-store-backend/spring-store/src/main/java/com/badas/springboot/service/GoogleOAuth2UserService.java
@@ -0,0 +1,11 @@
+package com.badas.springboot.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoogleOAuth2UserService implements Oauth2User{
+
+	public OAuth2User loaduser(OAuth2UserRequest userRequest) throws
+	
+}
